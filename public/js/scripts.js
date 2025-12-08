c30c70731468818508d82663947d86c97a7c7c43
Raquel <raquellgarcia97@gmail.com>
2025-12-08 00:08:44 -0800

    Add scripts.js file

---
+++ public/js/scripts.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', () =>{
+    init();
+});
+
+//Initialize App
+function init(){
+    //Functions to be implemented later
+    setupForms();
+    setupLocalStorage();
+    setupEventListeners();
+}
+
+function setupForms(){
+    console.log("Forms like login, signup, add workout, edit routine will be handled here");
+}
+
+function setupLocalStorage(){
+    console.log("LocalStorage integration");
+}
+
+function setupEventListeners(){
+    console.log("Event listeners for buttons, and dropdowns");
+}
