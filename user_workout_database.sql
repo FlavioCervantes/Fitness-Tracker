fde99027c5c0b59ca07c363695e2a45d19c1e833
Jeremy Jun <59491456+Jerry196725@users.noreply.github.com>
2025-12-13 18:25:12 -0800

    Add files via upload

---
+++ user_workout_database.sql
@@ -0,0 +1,15 @@
+GRANT INSERT, UPDATE ON user_workout_database.* TO 'app_user'@'localhost';
+
+CREATE TABLE `users` (
+    `userId` mediumint(9) NOT NULL AUTO_INCREMENT,
+    `host` varchar(255) COLLATE utf8_unicode_ci NOT NULL UNIQUE,
+    `user` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+    `password` varchar(100) COLLATE utf8_unicode_ci NOT NULL UNIQUE,
+    `database` varchar(50) COLLATE utf8_unicode_ci NOT NULL, 
+    PRIMARY KEY (`userId`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+INSERT INTO `users` (`userId`, `host`, `user`, `password`, `database`) 
+VALUES (1, 'host', 'user1', 'password123', 'db');
+
+FLUSH PRIVILEGES;
